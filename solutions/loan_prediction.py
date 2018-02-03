4d4be98cbc661d23c20621f5c64cfc4a89eef4ed
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-03 12:38:14 +0100

    add code block 3.discretization of rankings

--- solutions/loan_prediction.py
+++ solutions/loan_prediction.py
@@ -50,6 +50,53 @@ for feature in numeric_feature:
 train_rank.to_csv(data_path+'train_x_rank.csv',index=None)
 test_rank.to_csv(data_path+'test_x_rank.csv',index=None)
 
-print (train_rank.shape)#(614, 7)
-print (test_rank.shape)#(614, 7)
+print (train_rank.shape)#(614, 6)
+print (test_rank.shape)#(367, 6)
+
+#####################################
+#   3.discretization of rankings    #
+#####################################
+#create discretization features
+
+train_x = train_rank.drop(['Loan_ID'],axis=1)
+test_x = test_rank.drop(['Loan_ID'],axis=1)
+train_unlabeled = train.drop(['Loan_Status'],axis=1)
+train_unlabeled_x =  train_unlabeled.drop(['Loan_ID'],axis=1)
+
+#discretization of train ranking features
+#each 10% belongs to 1 level
+train_x[train_x<int(len(train_rank)/10)] = 1
+train_x[(train_x>=int(len(train_rank)/10))&(train_x<int(len(train_rank)/10*2))] = 2
+train_x[(train_x>=int(len(train_rank)/10*2))&(train_x<int(len(train_rank)/10*3))] = 3
+train_x[(train_x>=int(len(train_rank)/10*3))&(train_x<int(len(train_rank)/10*4))] = 4
+train_x[(train_x>=int(len(train_rank)/10*4))&(train_x<int(len(train_rank)/10*5))] = 5
+train_x[(train_x>=int(len(train_rank)/10*5))&(train_x<int(len(train_rank)/10*6))] = 6
+train_x[(train_x>=int(len(train_rank)/10*6))&(train_x<int(len(train_rank)/10*7))] = 7
+train_x[(train_x>=int(len(train_rank)/10*7))&(train_x<int(len(train_rank)/10*8))] = 8
+train_x[(train_x>=int(len(train_rank)/10*8))&(train_x<int(len(train_rank)/10*9))] = 9
+train_x[train_x>=int(len(train_rank)/10*9)] = 10
+       
+#nameing rule for discretization features, add "d" in front of orginal features
+#for instance "x1" would have discretization feature of "dx1"
+rename_dict = {s:'d'+s[1:] for s in train_x.columns.tolist()}
+train_x = train_x.rename(columns=rename_dict)
+train_x['Loan_ID'] = train.Loan_ID
+train_x.to_csv(data_path+'train_x_discretization.csv',index=None)
+
+#discretization of test ranking features
+
+test_x[test_x<int(len(test_x)/10)] = 1
+test_x[(test_x>=int(len(test_x)/10))&(test_x<int(len(test_x)/10*2))] = 2
+test_x[(test_x>=int(len(test_x)/10*2))&(test_x<int(len(test_x)/10*3))] = 3
+test_x[(test_x>=int(len(test_x)/10*3))&(test_x<int(len(test_x)/10*4))] = 4
+test_x[(test_x>=int(len(test_x)/10*4))&(test_x<int(len(test_x)/10*5))] = 5
+test_x[(test_x>=int(len(test_x)/10*5))&(test_x<int(len(test_x)/10*6))] = 6
+test_x[(test_x>=int(len(test_x)/10*6))&(test_x<int(len(test_x)/10*7))] = 7
+test_x[(test_x>=int(len(test_x)/10*7))&(test_x<int(len(test_x)/10*8))] = 8
+test_x[(test_x>=int(len(test_x)/10*8))&(test_x<int(len(test_x)/10*9))] = 9
+test_x[test_x>=int(len(test_x)/10*9)] = 10
+
+test_x = test_x.rename(columns=rename_dict)
+test_x['Loan_ID'] = test.Loan_ID
+test_x.to_csv(data_path+'test_x_discretization.csv',index=None)
 
