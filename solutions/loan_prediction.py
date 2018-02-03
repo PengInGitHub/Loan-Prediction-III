11e150624afeea8f1638b32b45eaad4df969f1ea
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-03 12:57:54 +0100

    add code block 4.frequency of ranking discretization

--- solutions/loan_prediction.py
+++ solutions/loan_prediction.py
@@ -100,3 +100,31 @@ test_x = test_x.rename(columns=rename_dict)
 test_x['Loan_ID'] = test.Loan_ID
 test_x.to_csv(data_path+'test_x_discretization.csv',index=None)
 
+#############################################
+#   4.frequency of ranking discretization   #
+#############################################
+
+#count of discretization of rankings
+train_x['n1'] = (train_x==1).sum(axis=1)
+train_x['n2'] = (train_x==2).sum(axis=1)
+train_x['n3'] = (train_x==3).sum(axis=1)
+train_x['n4'] = (train_x==4).sum(axis=1)
+train_x['n5'] = (train_x==5).sum(axis=1)
+train_x['n6'] = (train_x==6).sum(axis=1)
+train_x['n7'] = (train_x==7).sum(axis=1)
+train_x['n8'] = (train_x==8).sum(axis=1)
+train_x['n9'] = (train_x==9).sum(axis=1)
+train_x['n10'] = (train_x==10).sum(axis=1)
+train_x[['Loan_ID','n1','n2','n3','n4','n5','n6','n7','n8','n9','n10']].to_csv(data_path+'train_x_nd.csv',index=None)
+
+test_x['n1'] = (test_x==1).sum(axis=1)
+test_x['n2'] = (test_x==2).sum(axis=1)
+test_x['n3'] = (test_x==3).sum(axis=1)
+test_x['n4'] = (test_x==4).sum(axis=1)
+test_x['n5'] = (test_x==5).sum(axis=1)
+test_x['n6'] = (test_x==6).sum(axis=1)
+test_x['n7'] = (test_x==7).sum(axis=1)
+test_x['n8'] = (test_x==8).sum(axis=1)
+test_x['n9'] = (test_x==9).sum(axis=1)
+test_x['n10'] = (test_x==10).sum(axis=1)
+test_x[['Loan_ID','n1','n2','n3','n4','n5','n6','n7','n8','n9','n10']].to_csv(data_path+'test_x_nd.csv',index=None)
