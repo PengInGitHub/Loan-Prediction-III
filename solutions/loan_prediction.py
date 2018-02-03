82b8672b2c8cc85bd24e0e486d101fa3567df2c2
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-03 13:45:10 +0100

    add code block:5.feature importance of rank features

--- solutions/loan_prediction.py
+++ solutions/loan_prediction.py
@@ -128,3 +128,136 @@ test_x['n8'] = (test_x==8).sum(axis=1)
 test_x['n9'] = (test_x==9).sum(axis=1)
 test_x['n10'] = (test_x==10).sum(axis=1)
 test_x[['Loan_ID','n1','n2','n3','n4','n5','n6','n7','n8','n9','n10']].to_csv(data_path+'test_x_nd.csv',index=None)
+
+##############################################
+#   5.feature importance of rank features    #
+##############################################
+#generate a variety of xgboost models to have rank feature importance
+
+import xgboost as xgb
+import sys
+import _pickle as cPickle
+import os
+
+#load data
+train_x = pd.read_csv(data_path+"train_x_rank.csv")
+train_label=train[['Loan_Status','Loan_ID']]
+#convert Y to 0 and N to 1
+train_label['Loan_Status'] = train_label['Loan_Status'].map({'Y': 0, 'N': 1})
+
+train_xy = pd.merge(train_x,train_label,on='Loan_ID')
+y = train_xy.Loan_Status
+
+#leave features only
+train_x= train_xy.drop(["Loan_ID",'Loan_Status'],axis=1)
+#convert to percentage 
+X = train_x/len(train_x)
+#to xgb.DMatrix format
+dtrain = xgb.DMatrix(X, label=y)
+
+#do the same to test table    
+test = pd.read_csv(data_path+"test_x_rank.csv")
+test_uid = test.Loan_ID
+test = test.drop("Loan_ID",axis=1)
+test_x = test/len(test)
+dtest = xgb.DMatrix(test_x)
+
+#define an xgb model to do calculate feature importance 
+def pipeline(iteration,random_seed,gamma,max_depth,lambd,subsample,colsample_bytree,min_child_weight):
+    params={
+    	'booster':'gbtree',
+    	'objective': 'binary:logistic',
+    	#'scale_pos_weight': float(len(y)-sum(y))/float(sum(y)),
+        'eval_metric': 'error',
+    	'gamma':gamma,
+    	'max_depth':max_depth,
+    	'lambda':lambd,
+        'subsample':subsample,
+        'colsample_bytree':colsample_bytree,
+        'min_child_weight':min_child_weight, 
+        'eta': 0.04,
+    	'seed':random_seed,
+        }
+    
+    watchlist  = [(dtrain,'train')]
+    model = xgb.train(params,dtrain,num_boost_round=30000,evals=watchlist)
+    model.save_model('./model/xgb{0}.model'.format(iteration))
+    
+    #predict test set
+    test_y = model.predict(dtest)
+    test_result = pd.DataFrame(columns=["Loan_ID","score"])
+    test_result.uid = test_uid
+    test_result.score = test_y
+    test_result.to_csv("./preds/xgb{0}.csv".format(iteration),index=None,encoding='utf-8')
+    
+    #save feature score
+    feature_score = model.get_fscore()
+    feature_score = sorted(feature_score.items(), key=lambda x:x[1],reverse=True)
+    fs = []
+    for (key,value) in feature_score:
+        fs.append("{0},{1}\n".format(key,value))
+    
+    with open('./featurescore/feature_score_{0}.csv'.format(iteration),'w') as f:
+        f.writelines("feature,score\n")
+        f.writelines(fs)
+
+
+
+if __name__ == "__main__":
+    random_seed = list(range(1000,2000,10))
+    gamma = [i/1000.0 for i in list(range(100,200,1))]
+    max_depth = [6,7,8]
+    lambd = list(range(100,200,1))
+    subsample = [i/1000.0 for i in list(range(500,700,2))]
+    colsample_bytree = [i/1000.0 for i in list(range(250,350,1))]
+    min_child_weight = [i/1000.0 for i in list(range(200,300,1))]
+    random.shuffle(random_seed)
+    random.shuffle(gamma)
+    random.shuffle(max_depth)
+    random.shuffle(lambd)
+    random.shuffle(subsample)
+    random.shuffle(colsample_bytree)
+    random.shuffle(min_child_weight)
+    
+#save params for reproducing
+    with open('params.pkl','wb') as f:
+        cPickle.dump((random_seed,gamma,max_depth,lambd,subsample,colsample_bytree,min_child_weight),f)
+    
+
+#train 100 xgb
+    for i in list(range(100)):
+        pipeline(i,random_seed[i],gamma[i],max_depth[i%3],lambd[i],subsample[i],colsample_bytree[i],min_child_weight[i])
+
+
+#calculate average feature score for ranking features
+
+#get rank feature importance info from the xgboost models
+
+#featurescore folder contains csv files called feature_score_* that tells feature importance
+
+files = os.listdir('featurescore')
+#save into a dict
+fs = {}
+for f in files:
+    t = pd.read_csv('featurescore/'+f)
+    t.index = t.feature
+    t = t.drop(['feature'],axis=1)
+    d = t.to_dict()['score']
+    for key in d:
+        if key in fs:
+            fs[key] += d[key]
+        else:
+            fs[key] = d[key] 
+       
+#sort and organize the dict            
+fs = sorted(fs.items(), key=lambda x:x[1],reverse=True)
+
+t = []
+type(t)
+for (key,value) in fs:
+    t.append("{0},{1}\n".format(key,value))
+
+#save the overall importance scores of ranking features into csv
+with open('rank_feature_score.csv','w') as f:
+    f.writelines("feature,score\n")
+    f.writelines(t)
